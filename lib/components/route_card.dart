375de5bcdb481c5707b759f53ba193c4681b1e24
Huzaifa Rashid <huzaifarashid3@gmail.com>
2024-08-09 18:27:38 +0500

    limited letters display on route. tap to show

--- lib/components/route_card.dart
+++ lib/components/route_card.dart
@@ -8,28 +8,31 @@ class RouteCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int n = loc.length;
-    return SizedBox(
-      height: 20,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: n,
-        itemBuilder: (context, i) => Row(
-          children: [
-            Container(
-                height: 20,
-                padding: EdgeInsets.symmetric(horizontal: 4),
-                decoration: BoxDecoration(
-                  color: Colors.red[300],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: AnimatedText(loc: loc[i])),
-            if (i < n - 1)
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: SizedBox(
+        height: 20,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: n,
+          itemBuilder: (context, i) => Row(
+            children: [
               Container(
-                height: 2,
-                width: 15,
-                color: Colors.grey[600],
-              ),
-          ],
+                  height: 20,
+                  padding: EdgeInsets.symmetric(horizontal: 4),
+                  decoration: BoxDecoration(
+                    color: Colors.red[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: AnimatedText(loc: loc[i])),
+              if (i < n - 1)
+                Container(
+                  height: 2,
+                  width: 15,
+                  color: Colors.grey[600],
+                ),
+            ],
+          ),
         ),
       ),
     );
@@ -50,6 +53,7 @@ class AnimatedText extends StatefulWidget {
 
 class _AnimatedTextState extends State<AnimatedText> {
   bool condensed = true;
+  static const int n = 6;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -60,7 +64,7 @@ class _AnimatedTextState extends State<AnimatedText> {
         duration: Duration(milliseconds: 100),
         child: Center(
             child: Text(
-                " ${widget.loc.substring(0, min(condensed ? 7 : 1000, widget.loc.length))} ")),
+                " ${widget.loc.substring(0, min(condensed ? n : 1000, widget.loc.length))} ")),
       ),
     );
   }
