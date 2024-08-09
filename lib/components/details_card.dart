3ba9449ffe4c30ac0203a0f2bf12d34998636c30
Huzaifa Rashid <huzaifarashid3@gmail.com>
2024-08-09 16:41:57 +0500

    info card color tweak

--- lib/components/details_card.dart
+++ lib/components/details_card.dart
@@ -148,21 +148,21 @@ class InfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle style = TextStyle(fontSize: 12);
+    final TextStyle style = TextStyle(fontSize: 12, color: Colors.white);
     return Row(children: [
       Container(
         padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
         decoration: BoxDecoration(
-          color: Colors.grey[300],
+          color: Colors.grey[700],
           borderRadius: BorderRadius.circular(10),
         ),
-        child: Text(name.toUpperCase(), style: style),
+        child: Text(name, style: style),
       ),
       SizedBox(width: 10),
       Container(
         padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
         decoration: BoxDecoration(
-          color: Colors.grey[300],
+          color: Colors.grey[700],
           borderRadius: BorderRadius.circular(10),
         ),
         child: Text(contact, style: style),
